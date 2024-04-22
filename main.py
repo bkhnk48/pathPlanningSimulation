3dd7e43897d726a5dd4d9d71e3f3d99a94ad0965
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-04-22 10:26:52 +0700

    Update main.py
    gom 2 đoạn code đọc file lại làm một

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def getReal():
 def getForecast():
     return 17
 
-AGV = set()
+allAGVs = set()
 TASKS = set()
 
 x = {}
@@ -17,6 +17,7 @@ y = {}
 
 graph = Graph()  # Assuming a Graph class has appropriate methods to handle updates
 
+events = []
 # Mở file để đọc
 with open('TSG_0.txt', 'r') as f:
 	# Đọc từng dòng của file
@@ -26,15 +27,20 @@ with open('TSG_0.txt', 'r') as f:
     	# Kiểm tra loại dữ liệu của dòng
             if parts[0] == 'n':  # Nếu là dòng chứa thông tin về AGV hoặc công việc
                 if int(parts[2]) == 1:
-                    AGV.add(parts[1])  # Thêm vào tập hợp AGV
+		    node_id = int(parts[1])
+                    agv = AGV("AGV" + str(node_id), node_id)  # Create an AGV at this node
+                    start_event = StartEvent(0, agv, graph)  # Start event at time 0
+                    events.append(start_event)
+                    allAGVs.add(agv)  # Thêm vào tập hợp AGV
                 elif int(parts[2]) == -1:
                     TASKS.add(parts[1])  # Thêm vào tập hợp TASKS
             elif parts[0] == 'a':  # Nếu là dòng chứa thông tin về mối quan hệ
                     i, j, c_i_j = int(parts[1]), int(parts[2]), int(parts[5])
                     x[i, j] = c_i_j  # Lưu thông tin về mối quan hệ vào từ điển x
             graph.insertEdgesAndNodes(i, j, c_i_j)
+events = sorted(events, key=lambda x: x.startTime)
 
-def parse_tsg_file(filename):
+"""def parse_tsg_file(filename):
     events = []
     with open(filename, 'r') as f:
         for line in f:
@@ -46,6 +52,7 @@ def parse_tsg_file(filename):
                 events.append(start_event)
                 # We could potentially break here if we only expect one starting event or continue if multiple starts are possible
     return sorted(events, key=lambda x: x.startTime)
+"""
 
 def schedule_events(events):
     for event in events:
@@ -54,6 +61,6 @@ def schedule_events(events):
 # Main execution
 if __name__ == "__main__":
     simulator.ready()
-    events = parse_tsg_file('TSG_0.txt')
+    #events = parse_tsg_file('TSG_0.txt')
     schedule_events(events)
     simulator.run()
