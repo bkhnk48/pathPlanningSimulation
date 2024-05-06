b2e4fc904783a951a68f0f0369a5bd9f9ca54248
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-05-06 10:25:34 +0700

    Subclass of Node
    The artificial one

---
+++ model/ArtificialNode.py
@@ -0,0 +1,7 @@
+class ArtificialNode(Node):
+    def __init__(self, id, label=None, temporary=False):
+        super().__init__(id, label)
+        self.temporary = temporary  # Indicates whether the node is temporary
+
+    def __repr__(self):
+        return f"ArtificialNode(id={self.id}, label='{self.label}', temporary={self.temporary})"
