2750d2ea8eb7c8b0a1708e818c2f82d701a01b27
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-05-12 20:40:04 +0700

    pass two params instead of one

--- model/Node.py
+++ model/Node.py
@@ -1,6 +1,7 @@
 class Node:
-    def __init__(self, ID):
-        self.ID = ID
+    def __init__(self, id, label=None):
+        self.id = id
+        self.label = label
 
     def __repr__(self):
         return f"Node(id={self.id}, label='{self.label}')"
