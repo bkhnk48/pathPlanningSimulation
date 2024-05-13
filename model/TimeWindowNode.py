469371d5539751f56bc51d3e8a7022f9157cf343
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-05-14 02:00:46 +0700

    Create TimeWindowNode.py
    theo BT sau: https://docs.google.com/document/d/1P_EHhCMNBisby9_SkvThaRvXZPHUP-7ZWZhWJIOtkvY/edit?usp=sharing

---
+++ model/TimeWindowNode.py
@@ -0,0 +1,9 @@
+class TimeWindowNode(Node):
+    def __init__(self, id, label=None):
+        super().__init__(id,label)
+
+    def create_edge(self, node, M, d, e):
+        if isinstance(node, TimeWindowNode):
+            return TimeWindowEdge(self, node, e[4], "TimeWindows")
+        else:
+            return None
