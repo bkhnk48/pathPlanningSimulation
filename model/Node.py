b186bd9aafd9e20622b9201b4548b576743a5e7b
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-05-14 01:58:51 +0700

    cài đặt phần 1 của bt sau
    https://docs.google.com/document/d/1P_EHhCMNBisby9_SkvThaRvXZPHUP-7ZWZhWJIOtkvY/edit?usp=sharing

--- model/Node.py
+++ model/Node.py
@@ -1,7 +1,21 @@
 class Node:
-    def __init__(self, id, label=None):
+    def __init__(self, id,label=None):
         self.id = id
-        self.label = label
+        self.label=label
+        self.edges = []
 
+    def create_edge(self, node, M, d, e):
+        if node.id % M == self.id % M and ((node.id - self.id) // M == d) and isinstance(node, Node) and not isinstance(node, RestrictionNode) and not isinstance(node, TimeWindowNode)::
+            return HoldingEdge(self, node, d, d)
+        elif isinstance(node, RestrictionNode):
+            return RestrictionEdge(self, node, e[4], "Restriction")
+        elif isinstance(node, TimeWindowNode):
+            return TimeWindowEdge(self, node, e[4], "TimeWindows")
+        elif isinstance(node, Node):
+            if node.id % M != self.id % M:
+                return MovingEdge(self, node)
+        else:
+            return None
+    
     def __repr__(self):
-        return f"Node(id={self.id}, label='{self.label}')"
+        return f"Node(id={self.id}, label='{self.label}')"   
