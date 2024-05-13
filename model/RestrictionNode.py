2d897fb1916a09222a3ab768bc96f24189408ef9
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-05-14 01:59:48 +0700

    Create RestrictionNode.py
    theo BT sau: https://docs.google.com/document/d/1P_EHhCMNBisby9_SkvThaRvXZPHUP-7ZWZhWJIOtkvY/edit?usp=sharing

---
+++ model/RestrictionNode.py
@@ -0,0 +1,9 @@
+class RestrictionNode(Node):
+    def __init__(self, id, label=None):
+        super().__init__(id,label)
+
+    def create_edge(self, node, M, d, e):
+        if isinstance(node, RestrictionNode):
+            return RestrictionEdge(self, node, e[4], "Restriction")
+        else:
+            return None
