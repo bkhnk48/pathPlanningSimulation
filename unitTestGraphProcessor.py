fd6d977f77beb42d97a1162051dda97156e06664
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-05-22 03:22:25 +0700

    prepare for new assert (tbc)

--- unitTestGraphProcessor.py
+++ unitTestGraphProcessor.py
@@ -56,6 +56,21 @@ assert (graph.count_edges() == len(processor.ts_edges)), "Missing some edges els
 assert (len(graph.nodes) == len(processor.ts_nodes)), f"Missing some nodes elsewhere as {len(graph.nodes)} != {len(processor.ts_nodes)}"
 
 processor.update_file(id1 = 1, id2 = 8, c12 = 3)
+""" Cần có các assert như sau:
+(1) Tất cả các Node (không kể các TimeWindowNode và RestrictionNode) mà có time >= thời điểm hiện tại* thì bằng 24
+(2) Tất cả các Edge (không kể các TimeWindowEdge và RestrictionEdge) thì đỉnh nguồn của chúng phải có time >= thời điểm hiện tại*
+(3) Tất cả các TimeWindowNode thì vẫn còn các đỉnh khác nối đến chúng
+(4) Tất cả các RestrictionNode thì vẫn còn đỉnh nối đến chúng
+(5) Số các TimeWindowNode sẽ bằng 2
+(6) Số các TimeWindowEdge mới sẽ bằng số TimeWindowsEdge cũ trừ đi số TimeWindowEdge có đỉnh nguồn với thời điểm của đỉnh nguồn < thời điểm hiện tại
+(7) Số các RestrictionEdge mới (có đỉnh nguồn không phải RestrictionNode) sẽ bằng số
+     RestrictionEdge (có đỉnh nguồn không phải RestrictionNode) cũ trừ đi 
+     số RestrictionEdge có đỉnh nguồn (không phải RestrictionNode) với thời điểm của đỉnh nguồn < thời điểm hiện tại
+(8) Số các RestrictionEdge mới (có đỉnh đích không phải RestrictionNode) sẽ bằng số
+     RestrictionEdge (có đỉnh đích không phải RestrictionNode) cũ trừ đi 
+     số RestrictionEdge có đỉnh đích (không phải RestrictionNode) với thời điểm của đỉnh đích < thời điểm hiện tại     
+*thời điểm hiện tại: có công thức tính thời điểm hiện tại từ tham số của hàm update_file 
+"""
 graph.update(currentpos = 1, nextpos = 8, realtime = 3)
 #graph.update_file(id1 = 1, id2 = 8, c12 = 3)
 
