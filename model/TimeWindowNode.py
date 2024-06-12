316a689d72da60ad5e188abd41e38ce6aa4bd7ff
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-06-12 16:31:01 +0700

    add two new attributes

--- model/TimeWindowNode.py
+++ model/TimeWindowNode.py
@@ -4,7 +4,13 @@ class TimeWindowNode(Node):
     def __init__(self, ID, time_window):
         super().__init__(ID)
         self.time_window = time_window  # Time window in which the node can be accessed
+        self.earliness = float('-inf')
+        self.tardiness = float('inf')
 
+    def set_time_window(self, earliness, tardiness):
+        self.earliness = earliness
+        self.tardiness = tardiness
+        
     def create_edge(self, node, M, d, e):
         # Does nothing and returns None, effectively preventing the creation of any edge.
         return None
