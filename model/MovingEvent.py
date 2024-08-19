f3d368cb152fa7888741a1fc72bbb5ddc9c5bb50
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-08-19 14:51:57 +0700

    prevent from accessing unavailable nodes

--- model/MovingEvent.py
+++ model/MovingEvent.py
@@ -1,4 +1,5 @@
 from .Event import Event
+import inspect
 import pdb
 class MovingEvent(Event):
     def __init__(self, startTime, endTime, agv, graph, start_node, end_node):
@@ -6,35 +7,54 @@ class MovingEvent(Event):
         #pdb.set_trace()
         self.start_node = start_node
         self.end_node = end_node
+        M = self.graph.numberOfNodesInSpaceGraph
+        t1 = self.start_node // M - (1 if self.start_node % M == 0 else 0)
+        if(t1 != self.startTime):
+            if(self.graph.graph_processor.printOut):
+                print("Errror")
+            pdb.set_trace()
+            # Lấy thông tin về khung hiện tại
+            current_frame = inspect.currentframe()
+            # Lấy tên của hàm gọi my_function
+            caller_name = inspect.getframeinfo(current_frame.f_back).function
+            if(self.graph.graph_processor.printOut):
+                print(f'MovingEvent.py:19 {caller_name}')
 
     def updateGraph(self):
         actual_time = self.endTime - self.startTime
-        pdb.set_trace()
+        #pdb.set_trace()
         #if(self.start_node == 10 or self.agv.id == 'AGV10'):
         #    pdb.set_trace()
         #weight_of_edge = self.graph.get_edge(self.start_node, self.end_node)  # Use self.graph instead of Graph
         M = self.graph.numberOfNodesInSpaceGraph
         t2 = self.end_node // M - (1 if self.end_node % M == 0 else 0)
         t1 = self.start_node // M - (1 if self.start_node % M == 0 else 0)
+        #if(t1 != self.startTime):
+        #    pdb.set_trace()
         real_end_node = self.endTime*M + (M if self.end_node % M == 0 else self.end_node % M)
         self.agv.path.add(real_end_node)
         
         if(real_end_node in self.graph.nodes):
+            self.graph.nodes[real_end_node].agv = self.agv
+        if self.start_node in self.graph.nodes:
+            self.graph.nodes[self.start_node].agv = None
+            """pdb.set_trace()
             self.graph.nodes[real_end_node].agv = self.graph.nodes[self.start_node].agv \
                 if(self.graph.nodes[self.start_node].agv is not None) else self.graph.nodes[self.end_node].agv
-        self.graph.nodes[self.start_node].agv = None
+        self.graph.nodes[self.start_node].agv = None"""
         
         weight_of_edge = t2 - t1
         predicted_time = weight_of_edge or None
-        if(real_end_node == 13899):
-            pdb.set_trace()
+        #if(real_end_node == 14987):
+        #    pdb.set_trace()
         #pdb.set_trace()
 
         if actual_time != predicted_time:
-            self.graph.nodes[self.end_node].agv = None
+            if self.end_node in self.graph.nodes:
+                self.graph.nodes[self.end_node].agv = None
             self.graph.update_graph(self.start_node, self.end_node, actual_time, self.agv.id)
-            pdb.set_trace()
-            self.agv.traces = [self.graph.nodes[real_end_node]]
+            #pdb.set_trace()
+            self.agv.set_traces([self.graph.nodes[real_end_node]])
             self.graph.nodes[real_end_node].agv = self.agv
             #self.graph.update_edge(self.start_node, self.end_node, actual_time)  # Use self.graph instead of Graph
             #self.graph.handle_edge_modifications(self.start_node, self.end_node, self.agv)  # Use self.graph instead of Graph
