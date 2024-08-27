b982ba768bf22d1c73da239d328f4b75682d6995
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-08-27 23:03:51 +0700

    preparing for debugging

--- model/RestrictionEdge.py
+++ model/RestrictionEdge.py
@@ -1,9 +1,15 @@
 from .Edge import Edge
-
+import inspect
+import pdb
 class RestrictionEdge(Edge):
     def __init__(self, start_node, end_node, edge, label):
         super().__init__(start_node, end_node, edge[2], edge[3], edge[4])
         self.label = label
+        """current_frame = inspect.currentframe()
+        # Lấy tên của hàm gọi my_function
+        caller_name = inspect.getframeinfo(current_frame.f_back).function
+        #if(self.graph.graph_processor.printOut):
+        print(f'RestrictionEdge.py:12 {caller_name}')"""
 
     def make_permanent(self):
         # This method could be used to convert a temporary edge into a permanent one
