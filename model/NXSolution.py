4fea500a21a0c9012e3039d154b57eb8e3522950
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-09-20 17:39:04 +0700

    write traces.txt

--- model/NXSolution.py
+++ model/NXSolution.py
@@ -1,7 +1,16 @@
 import networkx as nx
 import pdb
+from collections import defaultdict
 
 class NetworkXSolution:
+    def __init__(self):#, edges_with_costs, started_nodes, target_nodes):
+        self.started_nodes = None #started_nodes
+        self.target_nodes = None #target_nodes
+        self.edges_with_costs = None #edges_with_costs
+        self.flowCost = 0
+        self.flowDict = defaultdict(list)
+        self.M = 0
+    
     def read_dimac_file(self, file_path):
         G = nx.DiGraph()
         #pdb.set_trace()
@@ -18,10 +27,31 @@ class NetworkXSolution:
                     U = int(parts[4])
                     C = int(parts[5])
                     G.add_edge(ID1, ID2, weight=C, capacity=U)
-        flowCost, flowDict = nx.network_simplex(G)
-        return [flowCost, flowDict]
+        self.flowCost, self.flowDict = nx.network_simplex(G)
+        #print(type(self.flowDict))
+        #return [flowCost, flowDict]
         """print("flowCost:", flowCost)
         print("flowDict:", flowDict)"""
+    def write_trace(self, file_path = 'traces.txt'):
+        
+        filtered_data = {}
+        for key, sub_dict in self.flowDict.items():
+            # Lọc các phần tử có giá trị khác 0
+            filtered_sub_dict = {k: v for k, v in sub_dict.items() if v != 0}
+            if filtered_sub_dict:
+                filtered_data[key] = filtered_sub_dict
+        self.flowDict = filtered_data
+
+        with open(file_path, "w") as file:
+            for key, value in self.flowDict.items():
+                for inner_key, inner_value in value.items():
+                    if(inner_value > 0):
+                        s = int(key) // self.M + (self.M if int(key) // self.M == 0 else 0)
+                        t = int(inner_key) // self.M + (self.M if int(inner_key) // self.M == 0 else 0)
+                        cost = self.edges_with_costs.get((s, t), [-1, -1])[1]
+                        result = inner_value*cost
+                        #print(f"a {key} {inner_key} 0 + {result} = {result}")
+                        file.write(f"a {key} {inner_key} 0 + {result} = {result}\n")
 
 # Đường dẫn đến file DIMAC của bạn
 """file_path = input("Path to DIMAC file:")
