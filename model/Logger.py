c3d750433ca241738b258c1d7e0a972d063b320c
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-09-27 16:05:17 +0700

    get the number of rows

--- model/Logger.py
+++ model/Logger.py
@@ -3,18 +3,43 @@ import csv
 
 class Logger:
     def __init__(self):
+        self.index = 0
         pass
     def count_csv_rows(self, fileName):
         with open(fileName, mode='r', newline='') as file:
             reader = csv.reader(file)
             row_count = sum(1 for row in reader)
         return row_count
+    
+    def get_max_value(self, fileName, columnName):
+        try:
+            with open(fileName, mode='r', newline='') as file:
+                reader = csv.DictReader(file)
+                max_value = None
+                
+                for row in reader:
+                    if columnName in row and row[columnName]:
+                        value = float(row[columnName])
+                        if max_value is None or value > max_value:
+                            max_value = value
+                return max_value if max_value is not None else 0
+        except FileNotFoundError:
+            #print(f"File {fileName} không tồn tại.")
+            return 0
+        except ValueError:
+            return 0
+
+# Ví dụ sử dụng
+#fileName = 'data.csv'
+#columnName = 'name'
+#print(f'Giá trị lớn nhất trong cột {columnName}: {get_max_value(fileName, columnName)}')
+
 
     def log(self, fileName, map, totalAGVs, H, d, solution, reachingTarget, halting, objectiveValue, runTime):
         # Kiểm tra xem fileName có đuôi .csv chưa, nếu chưa thì thêm vào
         if not fileName.endswith('.csv'):
             fileName += '.csv'
-        initialIndex = 1
+        #initialIndex = 1
 
         # Kiểm tra xem file đã tồn tại chưa
         file_exists = os.path.isfile(fileName)
@@ -29,7 +54,8 @@ class Logger:
                                  'Num of Reaching Target AGVs', 'Num of Halting AGVs', 
                                  'Objective Value', 'Running Time(s)'])
                 
-            initialIndex = self.count_csv_rows(fileName)
+            #initialIndex = self.count_csv_rows(fileName)
+            initialIndex = self.get_max_value(fileName, 'No') + 1
 
             # Thêm dòng dữ liệu mới
             writer.writerow([initialIndex, map, totalAGVs, H, d, solution, reachingTarget, halting, 
