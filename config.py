9d29f3e3acf16590e6a08dc807ca9076e6188485
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-09-27 13:55:12 +0700

    some new values

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
 solver_choice = 'network-simplex'
+startedNodes = []
+ID = []
+earliness = []
+tardiness = []
+filepath = ""
+H = 0
+d = 0
+numMaxAGVs = 0
+numOfAGVs = 0
+count = 0
+reachingTargetAGVs = 0
+haltingAGVs = 0
+totalCost = 0
